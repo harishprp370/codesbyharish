5edba560ce47aa28abbaf761e881b4b4945074e5
Harish N <harishprp370@gmail.com>
2025-11-01 12:16:45 +0530

    Update Security

--- show.php
+++ show.php
@@ -1,6 +1,6 @@
 <!-- henerate andshow password using password hash in php -->
 <?php
-$password = "Harish@370";
+$password = "1234";
 $hashedPassword = password_hash($password, PASSWORD_BCRYPT);
 echo "Original Password: " . htmlspecialchars($password) . "<br>";
 echo "Hashed Password: " . htmlspecialchars($hashedPassword) . "<br>";
